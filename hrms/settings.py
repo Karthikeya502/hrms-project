747904151c711e9dd28204aa7d5557f290b75c14
Karthikeya Sathelly <karthikeyasathelly@Karthikeyas-MacBook-Air.local>
2026-05-22 14:37:52 +0530

    Production settings

--- hrms/settings.py
+++ hrms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-03-=4kd_v5jb&rabu3be8@tojal=^n#r-ut=z#g+j6ju_eq*c%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['hrms-project-3-xf5p.onrender.com']
 
 
 # Application definition
